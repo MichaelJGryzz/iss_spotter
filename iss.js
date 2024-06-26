c9e0454447b6abb26f1ad30adfcef80a4fa8a9ff
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-26 15:37:28 -0400

    Create "iss.js" file to store the "fetchMyIP" function which asynchronously returns our IP Address using the "ipify" API. Add code to require (import) the "needle" library at the top of the file. Add a "get" request in the function to retrieve the users IP address using needle and the ipify API URL for retrieving our "IPv4" IP address in JSON format. Add additional error handling inside "fetchMyIP" function to handle when no error is sent by the request function but a non-200 HTTP status code comes back as the HTTP Response assuming server error.

---
+++ iss.js
@@ -0,0 +1,41 @@
+// import "needle" library
+const needle = require('needle');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  const url = `https://api.ipify.org?formal=json`;
+
+  // fetch the IP address data from ipify API
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      callback(error, null); // pass error to callback function
+      return;
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // debugging step
+    console.log(body);
+    // the body contains the IP address sent back from the ipify API server, parse it
+    const ip = body;
+
+    // pass the IP address to the callback function
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
