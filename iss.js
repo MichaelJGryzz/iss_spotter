69e47cedbc15b3c9e2075f5567ae60bc287d9ecc
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-27 02:58:06 -0400

    Add "fetchCoordsByIP" function that takes in an IP address and returns the latitude and longitude for it via a "get" request with "needle" and the "ipwho.is" API. Add error handling for both network issues (status code) and invalid IP addresses (body.success). Include a callback to log the resulting coordinates or error messages. Add "fetchCoordsByIP" function to "module.exports" at the bottom of the file to export the function.

--- iss.js
+++ iss.js
@@ -28,7 +28,7 @@ const fetchMyIP = function(callback) {
       return;
     }
 
-    // the body contains the IP address sent back from the ipify API server, parse it
+    // the body contains the IP address sent back from the ipify API server
     const ip = body.ip;
 
     // pass the IP address to the callback function
@@ -36,4 +36,50 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+
+const fetchCoordsByIP = function(ip, callback) {
+  // use request to fetch IP address from JSON API
+  const url =  `http://ipwho.is/${ip}`;
+
+  // fetch the coordinates for the IP affress from ipwho.is API
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      callback(error, null); // pass error to callback function
+      return;
+    }
+
+    //check if "success" is true or not
+    if (!body.success) {
+      const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${body.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // the body contains the coordinates sent back from the ipwho.is API server
+    const latitude = body.latitude;
+    const longitude = body.longitude;
+
+    // pass the cooordinates to the callback function
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
