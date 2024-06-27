93d66119835d71c98c94dd682d785b6a49c67c41
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-27 02:47:22 -0400

    Add "fetchCoordsByIP" function into the require call at the top of the "index.js" file. Add a non-assertion based test to use our (IPv4) IP address string returned from the "fetchMyIP" function and call the "fetchCoordsByIP" function to fetch the latitude and longitude for said IP address.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // the code below is temporary and can be commented out.
-// require (import) fetchMyIP function
-const { fetchMyIP } = require('./iss');
+// require (import) fetchMyIP and fetchCoordsByIP function
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 // testing fetchMyIP function
 fetchMyIP((error, ip) => {
@@ -10,4 +10,14 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+
+  // testing fetchCoordsByIP function
+  fetchCoordsByIP(ip, (error, coords) => {
+    if (error) {
+      console.log("It didn't work!" , error);
+      return;
+    }
+
+    console.log('It worked! Returned coordinates:' , coords);
+  });
 });
