d6c414ff1ddea2c5925257b4fc0c09d4bb5a272e
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-27 01:45:07 -0400

    Add detailed comments for "fetchMyIP" function require call and testing.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // the code below is temporary and can be commented out.
+// require (import) fetchMyIP function
 const { fetchMyIP } = require('./iss');
 
+// testing fetchMyIP function
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
