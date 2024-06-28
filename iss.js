ac8949a4cc65ebcca862e142b26afd845d50a8c8
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-27 22:21:57 -0400

    Add "fetchISSFlyOverTimes" function that takes in coordinates (latitude and longitude) and returns the flyover times of the ISS for them via a "get" request with "needle" and the "iss-flyover.herokuapp" API. Add error handling in the request callback before attempting to parse the JSON response. Add more error handling for network issues (status code). Include a callback to log the resulting flyover times or error messages. Add "fetchISSFlyOverTimes" function to "module.exports" at the bottom of the file to export the function.

--- iss.js
+++ iss.js
@@ -52,7 +52,7 @@ const fetchCoordsByIP = function(ip, callback) {
   // use request to fetch IP address from JSON API
   const url =  `http://ipwho.is/${ip}`;
 
-  // fetch the coordinates for the IP affress from ipwho.is API
+  // fetch the coordinates for the IP address from ipwho.is API
   needle.get(url, (error, response, body) => {
     if (error) {
       callback(error, null); // pass error to callback function
@@ -82,4 +82,41 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  // fetch the upcoming ISS flyover times for the provided coordinates from iss-flyover.herokuapp API
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      callback(error, null); // pass error to callback function
+      return;
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching ISS flyover times: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // the body contains the flyover times sent back from the iss-flyover.herokuapp API server
+    const flyOverTimes = body.response;
+
+    // pass the flyover times to the callback function
+    callback(null, flyOverTimes);
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
