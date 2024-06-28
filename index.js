c57313294eaad6f94d9780955f05706eee9377e7
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-28 00:24:18 -0400

    Delete three previous require calls. Add require call for "nextISSTimesForMyLocation" function to import it from "iss.js" file. Add "printFlyOverTimes" function to format and log the resulting fly over times in the desired way according to Compass. Set up a callback using "nextISSTimesForMyLocation" function to handle errors and log the flyover times using "printFlyOverTimes".

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-// the code below is temporary and can be commented out.
-// require (import) fetchMyIP, fetchCoordsByIP and etchISSFlyOverTimes functions
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+// require (import) nextISSTimesForMyLocation function
+const { nextISSTimesForMyLocation } = require('./iss');
 
+/**
+ * Input:
+ *   Array of data objects defining the next fly-overs of the ISS.
+ *   [ { risetime: <number>, duration: <number> }, ... ]
+ * Returns:
+ *   undefined
+ * Sideffect:
+ *   Console log messages to make that data more human readable.
+ *   Example output:
+ *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+ */
+
+const printFlyOverTimes = function(flyOverTimes) {
+  for (const flyOver of flyOverTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(flyOver.risetime);
+    const duration = flyOver.duration;
+    console.log(`Next flyover at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, flyOverTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the details!
+  printFlyOverTimes(flyOverTimes);
+});
+
+// the code below is temporary and can be commented out.
 // testing fetchMyIP function
-fetchMyIP((error, ip) => {
+/*fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
@@ -30,4 +59,4 @@ fetchMyIP((error, ip) => {
       console.log('It worked! Returned flyover times:' , flyOverTimes);
     });
   });
-});
+});*/
